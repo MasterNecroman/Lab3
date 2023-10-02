0c1ca9278f625536bd81e5c2ed971f1f677c2233
MasterNecroman <{{fortunaq60@gmail.com}}>
2023-10-02 22:09:51 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Rectangle
+    {
+        public double Area { get; private set; }
+        public double Perimeter { get; private set; }
+
+        private double side1;
+        private double side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+
+            CalculateArea();
+            CalculatePerimeter();
+        }
+
+        private void CalculateArea()
+        {
+            Area = side1 * side2;
+        }
+
+        private void CalculatePerimeter()
+        {
+            Perimeter = 2 * (side1 + side2);
+        }
+    }
+}
